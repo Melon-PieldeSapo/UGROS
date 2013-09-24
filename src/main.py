99e3110cb9cfaee3d1203044a8fec80feee5d91a
Melon-PieldeSapo <hfazefiris@gmail.com>
2013-09-24 13:19:33 +0200

    funcionalidad en tabla
    asd

--- src/main.py
+++ src/main.py
@@ -8,17 +8,26 @@ from gi.repository import Gtk
 class gui(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="UGROS")
-        names = ["hola","adios","cosa"]
+        names = ["hola","adios","cosa","cosa fea","otra cosa","bieeen"]
         
-        self.box=Gtk.Box(spacing=6)
-        self.add(self.box)
+        #self.box=Gtk.Box(spacing=6)
+        #self.add(self.box)
+        self.table=Gtk.Table(2,3,True)
+        self.add(self.table)
+        i=0
+        j=0
         for name in names:
-            self.button = Gtk.Button(label=name)
-            self.button.connect("clicked", self.on_button_clicked)
-            self.box.pack_start(self.button, True, True, 0)
-
+            button = Gtk.Button(label=name)
+            button.connect("clicked", self.on_button_clicked)
+            #self.box.pack_start(self.button, True, True, 0)
+            self.table.attach(button,i,i+1,j,j+1)
+            i=i+1
+            if(i%2==0):
+                j=j+1
+                i=0
     def on_button_clicked(self, widget):
         print("Hello World")
+        print (widget)
 
 if __name__ == '__main__':
     win = gui()
